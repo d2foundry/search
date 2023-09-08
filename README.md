c49b00b50bb90fbc251f79cedb7dd60702746e63
Kat Michaela <k@gothfem.me>
2023-09-08 19:34:25 -0400

    feat: add example

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # @d2foundry/search
 
 Tools and configs for Foundry's Search
+
+## Goals
+
+1. make it easier to define filter keywords, and also expose it so i can get more people working on it
+2. have a neat way to transform/minify the bungie definitions into a space optimized/efficient search db to pipe over to the client
+3. have a neat way to map keywords to the search db on the client side. 
+   - could pre-process/transform the minimized format into one where all the values resolve to either strings or arrays of strings (a limitation of the current fuzzy search library)
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,35 @@
-export const keys = [];
+import { AllDestinyManifestComponents } from "bungie-api-ts/destiny2";
+
+type JsonValue = string | number | boolean | string[] | number[];
+type SearchValue = string | string[];
+
+interface SearchDbItem {
+  hash: number;
+  name: string;
+}
+
+// Definition for a "Filter Key"
+interface Key {
+  // filter keyword
+  name: string;
+  // takes inventoryItemHash and D2 Manifest Components,
+  // maps data relevant to this keyword to JSON-ok format
+  formatToDb: (
+    inventoryItemHash: number,
+    definitions: AllDestinyManifestComponents
+  ) => JsonValue;
+  // takes searchDbItem and maps to format ready for Fuzzy Search
+  // (may also need some additional parameter depending on how formatToDb goes)
+  getFromDb: (searchDbItem: SearchDbItem) => SearchValue;
+}
+
+export const keys: Key[] = [
+  {
+    name: "displayName",
+    formatToDb: (hash, definitions) => {
+      const item = definitions.DestinyInventoryItemDefinition[hash];
+      return item.displayProperties.name;
+    },
+    getFromDb: (item) => item.name,
+  },
+];
