715be724684af96c6d4658c53392c49d7c18d028
Kat Michaela <k@gothfem.me>
2023-09-10 17:44:48 -0400

    test: add more tests for utils

--- src/__test__/utils.test.ts
+++ src/__test__/utils.test.ts
@@ -1,5 +1,14 @@
 import { test, expect, describe } from "bun:test";
-import { getItemSource } from "src/utils";
+import {
+  getIsAdeptFromName,
+  getItemSource,
+  getAmmoType,
+  getEnergyFromDamageType,
+  getEventFromWatermark,
+  getRarityFromTierType,
+  getSlotFromSlotHash,
+  getSeasonNumberFromWatermark,
+} from "src/utils";
 
 const MISSING_SOURCE_ITEM = 4024037919; // origin story
 const PROPHECY_SOURCE_HASH = 506073192;
@@ -13,5 +22,40 @@ describe("getItemSource", () => {
   test("finds source info from source hash", () => {
     const source = getItemSource(MISSING_SOURCE_ITEM, PROPHECY_SOURCE_HASH);
     expect(source).toEqual(["dungeon", "prophecy"]);
+  });
+});
+
+describe("getIsAdeptFromName", () => {
+  test("returns true when Adept", () => {
+    expect(getIsAdeptFromName("The Palindrome (Adept)")).toBeTrue();
+    expect(getIsAdeptFromName("Fatebringer (Timelost)")).toBeTrue();
+  });
+  test("returns false when not Adept", () => {
+    expect(getIsAdeptFromName("The Palindrome")).toBeFalse();
+    expect(getIsAdeptFromName("Fatebringer")).toBeFalse();
+  });
+});
+
+describe("getAmmoType", () => {
+  test("gets ammo text from AmmoType", () => {
+    const source = getAmmoType(1);
+    expect(source).toBe("Primary");
+  });
+});
+
+describe("getEnergyFromDamageType", () => {
+  test("gets energy text from DamageType", () => {
+    const energy = getEnergyFromDamageType(3);
+    expect(energy).toBe("solar");
+  });
+});
+
+describe("getRarityFromTierType", () => {
+  test("gets energy text from DamageType", () => {
+    const exotic = getRarityFromTierType(6);
+    const legendary = getRarityFromTierType(5);
+
+    expect(exotic).toBe("exotic");
+    expect(legendary).toBe("legendary");
   });
 });
