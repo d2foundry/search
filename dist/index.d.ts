535b1b584b13de9c4565b28da1a0a6976767a2b4
gothfemme <gothfemme@users.noreply.github.com>
2023-09-15 21:18:08 +0000

    Apply formatting changes

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,4 @@
 export { keywordDictionary } from "./keywords";
 export type { KeywordDefinition, KeywordDefinitionDictionary, SearchKeywords, } from "./keywords";
 export { decompressJson, compressJson } from "./utils";
+export { buildSearchDatabase } from "./api";
