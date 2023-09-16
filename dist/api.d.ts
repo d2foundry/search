a4d5e5291b852ed462d78fa93cce87966911625f
gothfemme <gothfemme@users.noreply.github.com>
2023-09-16 17:38:33 +0000

    Apply formatting changes

--- dist/api.d.ts
+++ dist/api.d.ts
@@ -1,6 +1,6 @@
 import { AllDestinyManifestComponents, DestinyManifestSlice, HttpClientConfig } from "bungie-api-ts/destiny2";
 import { MANIFEST_SLICES } from "./constants";
-export declare function buildSearchDatabase(defs: AllDestinyManifestComponents): import("./types").SearchDbItem[];
+export declare function buildSearchDatabase(defs: AllDestinyManifestComponents): import("./types").SearchDbItemWithMetadata[];
 export declare function $http<T>(config: HttpClientConfig): Promise<T>;
 export declare function getManifest(): Promise<DestinyManifestSlice<typeof MANIFEST_SLICES>>;
-export declare function getSearchDb(): Promise<import("./types").SearchDbItem[]>;
+export declare function getSearchDb(): Promise<import("./types").SearchDbItemWithMetadata[]>;
